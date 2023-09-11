5771819ddec0148affe42611f768d24b08a60de3
d <dommyv@gmail.com>
2023-09-11 12:13:38 -0700

    uri update

--- config/database.js
+++ config/database.js
@@ -3,7 +3,10 @@ mongoose.set('strictQuery', false);
 
 const connectDB = async () => {
 	try {
-		const conn = await mongoose.connect(process.env.MONGO_URI.toString())
+		const conn = await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    })
 		console.log(`MongoDB connected: ${conn.connection.host}`)
 
 	} catch (err) {
